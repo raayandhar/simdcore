7a91ce68714d88a010a5b771055f0f5a3d07c157
Christopher Milan <chrismilan@ucla.edu>
2025-05-14 12:30:45 -0700

    cleanup

--- sim.py
+++ sim.py
@@ -1,13 +1,15 @@
 import struct, sys
 import numpy as np
 
-def as_np(arr: bytearray, dtype:np.dtype) -> np.ndarray: return np.frombuffer(arr, dtype=dtype)
+def as_np(arr: bytearray) -> np.ndarray: return np.frombuffer(arr, dtype=np.half)
 def from_np(arr: np.ndarray) -> bytearray: return arr.tobytes()
-def apply_func(func, *args) -> bytearray: return from_np(func(*[as_np(arg, np.half) for arg in args]))
+def apply_func(func, *args) -> bytearray: return from_np(func(*[as_np(arg) for arg in args]))
 def pack2(arg1: int, arg2: int) -> int: return arg1 << 8 | arg2
 
 SF = 0b00000001
 ZF = 0b00000010
+
+def get_flags(val:int) -> int: return ((val == 0x00 & ZF) | (val & 0x8000 & SF))
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
@@ -21,12 +23,6 @@ if __name__ == "__main__":
   sregs = [0] * 256
   vregs = [bytearray(16) for _ in range(256)]
   flags = 0
-
-  def set_flags(value: int):
-    global flags
-    value &= 0xFFFF
-    if value == 0:      flags |= ZF
-    if value & 0x8000:  flags |= SF
 
   while True:
   # for (opcode, arg1, arg2, arg3) in [struct.unpack("BBBB", b) for b in [data[i:i+4] for i in range(0, len(data), 4)]]:
@@ -64,7 +60,7 @@ if __name__ == "__main__":
       case "00010", "110": vregs[arg1] = apply_func(np.bitwise_xor, vregs[arg2], vregs[arg3])
       case "00010", "111": vregs[arg1] = apply_func(np.bitwise_not, vregs[arg2])
       case "00100", "000": # J (unconditional)
-        pc = pack2(arg1, arg2) 
+        pc = pack2(arg1, arg2)
         continue
       case "00100", "001": # JE (ZF == 1)
         if flags & ZF: pc = pack2(arg1, arg2)
@@ -84,19 +80,19 @@ if __name__ == "__main__":
       case "00100", "110": # JLT (SF == 1)
         if flags & SF: pc = pack2(arg1, arg2)
         continue
-      case "00101", "000": set_flags(sregs[arg1] - sregs[arg2])
-      case "00101", "001": set_flags(sregs[arg1] - pack2(arg2, arg3))
-      case "00101", "010": set_flags(pack2(arg1, arg2) - sregs[arg3])
+      case "00101", "000": flags = get_flags(sregs[arg1] - sregs[arg2])
+      case "00101", "001": flags = get_flags(sregs[arg1] - pack2(arg2, arg3))
+      case "00101", "010": flags = get_flags(pack2(arg1, arg2) - sregs[arg3])
       case "00101", "011": flags = arg1 & (SF | ZF)
     pc += 1
-    
-    if pc >= 3: break
+
+    if pc >= 0xf: break
   print("Sregs:")
   for i in range(5):
     print(f"s{i:2}: {sregs[i]:4}")
   print("Vregs:")
   for i in range(5):
-    print(f"v{i:2}: {as_np(vregs[i], np.half)}")
+    print(f"v{i:2}: {as_np(vregs[i])}")
 
 
 
